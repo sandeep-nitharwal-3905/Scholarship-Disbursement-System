4d0d8d1b642af882afddde7b946636c9347541f4
sanan-sajid <sanansajid015@gmail.com>
2024-09-27 17:02:35 +0530

    added login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import Dashboard from "./components/Dashboard";
-
+// import Login from "./pages/Login";
 const App = () => {
   return <Dashboard />;
+  // return <Login></Login>;
 };
 
 export default App;
