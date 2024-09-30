590bbaac9e5ead89c5f31dbf198ed3cb8627fe08
sanan-sajid <sanansajid015@gmail.com>
2024-09-30 18:56:22 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Analytics } from "@vercel/analytics/react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
