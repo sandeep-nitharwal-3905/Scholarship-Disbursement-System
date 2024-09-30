492ec71239a139af0eb1e14c1070526efac07afd
sanan-sajid <sanansajid015@gmail.com>
2024-09-30 19:01:15 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -21,45 +21,50 @@ import "react-toastify/dist/ReactToastify.css";
 import EditScholarship from "./components/EditScholarship";
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/admin" element={<AdminSignup />} />
-
-        <Route
-          path="*"
-          element={
-            <Layout>
-              <Routes>
-                <Route path="/dashboard" element={<Dashboard />} />
-                <Route path="/home" element={<Home />} />
-                <Route path="/upload" element={<UploadDocs />} />
-                <Route path="/track" element={<Track />} />
-                <Route path="/editScholarship" element={<EditScholarship />} />
-                <Route
-                  path="/admin-dashboard"
-                  element={<AddScholarshipForm />}
-                />
-                <Route path="/apply" element={<ScholarshipApplication />} />
-                <Route path="/admin-track" element={<ScholarshipList />} />
-                <Route
-                  path="/admin-scholarship-list"
-                  element={<ScholarshipList />}
-                />
-                <Route path="/viewScholarships" element={<StudentTrack />} />
-                <Route
-                  path="//admin-add-scholarship"
-                  element={<AddScholarshipForm />}
-                />
-              </Routes>
-            </Layout>
-          }
-        />
-      </Routes>
-      <ToastContainer />
+    <>
       <Analytics />
-    </Router>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/admin" element={<AdminSignup />} />
+
+          <Route
+            path="*"
+            element={
+              <Layout>
+                <Routes>
+                  <Route path="/dashboard" element={<Dashboard />} />
+                  <Route path="/home" element={<Home />} />
+                  <Route path="/upload" element={<UploadDocs />} />
+                  <Route path="/track" element={<Track />} />
+                  <Route
+                    path="/editScholarship"
+                    element={<EditScholarship />}
+                  />
+                  <Route
+                    path="/admin-dashboard"
+                    element={<AddScholarshipForm />}
+                  />
+                  <Route path="/apply" element={<ScholarshipApplication />} />
+                  <Route path="/admin-track" element={<ScholarshipList />} />
+                  <Route
+                    path="/admin-scholarship-list"
+                    element={<ScholarshipList />}
+                  />
+                  <Route path="/viewScholarships" element={<StudentTrack />} />
+                  <Route
+                    path="//admin-add-scholarship"
+                    element={<AddScholarshipForm />}
+                  />
+                </Routes>
+              </Layout>
+            }
+          />
+        </Routes>
+        <ToastContainer />
+      </Router>
+    </>
   );
 };
 
