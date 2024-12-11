d9cc070ca27f6aa7c8968749182bd69934025bc6
sanan-sajid <sanansajid015@gmail.com>
2024-12-11 22:07:38 +0530

    safe

--- src/components/NewTopBar.jsx
+++ src/components/NewTopBar.jsx
@@ -27,7 +27,7 @@ const NewTopBar = () => {
   ];
 
   return (
-    <div className="bg-gray-900 text-white w-full shadow-md">
+    <div className="bg-gray-900 text-white w-full shadow-md hidden sm:block">
       <div className="container mx-auto py-2">
         <ul className="flex flex-wrap justify-between">
           {menuItems.map((item, index) => (
