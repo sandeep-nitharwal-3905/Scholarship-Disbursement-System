69c9699c0cc827487c2a9fee64eb65587b6592fb
asishghos <ccasish01@gmail.com>
2024-12-12 13:16:18 +0530

    edit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -84,7 +84,7 @@ const AdminLogin = () => {
     e.preventDefault();
     try {
       const userData = await loginUser(email, password);
-      toast.success("Admin logged in successfully!");
+      toast.success("Institute Admin logged in successfully!");
       localStorage.setItem("userRole", "admin");
 
       setTimeout(() => {
@@ -102,19 +102,19 @@ const AdminLogin = () => {
   return (
     <div className="animate-fadeIn">
       <h2 className="text-2xl font-bold text-gray-800 mb-6 border-b pb-2 border-gray-300">
-        Admin Login
+      Institute Admin Login
       </h2>
       <form onSubmit={handleLogin}>
         <input
           type="email"
-          placeholder="Admin Email"
+          placeholder="Institute Admin Email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           className="w-full p-3 mb-4 rounded-lg border border-gray-300 focus:border-blue-400 focus:ring-4 focus:ring-blue-200 transition"
         />
         <input
           type="password"
-          placeholder="Admin Password"
+          placeholder="Institute Admin Password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           className="w-full p-3 mb-4 rounded-lg border border-gray-300 focus:border-blue-400 focus:ring-4 focus:ring-blue-200 transition"
@@ -123,14 +123,8 @@ const AdminLogin = () => {
           type="submit"
           className="w-full bg-gray-800 text-white py-3 rounded-lg mt-4 transition-transform transform hover:scale-105 active:scale-95"
         >
-          Login as Admin
+          Login as Institute Admin
         </button>
-        <Link
-          to="/signup"
-          className="block w-full bg-blue-600 text-white py-3 rounded-lg mt-4 transition-transform transform hover:scale-105 active:scale-95"
-        >
-          Sign Up
-        </Link>
       </form>
       <div className="bg-gray-100 p-4 rounded-lg mt-6 border border-gray-300 text-gray-700">
         <h3 className="font-semibold mb-2">Sample Login Credentials:</h3>
