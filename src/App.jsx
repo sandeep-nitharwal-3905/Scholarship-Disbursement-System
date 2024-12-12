79a3d74c57e43dcff51dbef8e29b168ac583eaed
Shibshankar0909 <123229119+Shibshankar0909@users.noreply.github.com>
2024-12-12 16:03:47 +0530

    added route

--- src/App.jsx
+++ src/App.jsx
@@ -33,6 +33,7 @@ import "./App.css";
 import AboutPage from "./pages/Home_About";
 import ProtectedRoute from "./pages/ProtectedRoutes";
 import ForbiddenPage from "./pages/forbidden";
+import Contactd from "./pages/contactd";
 
 const App = () => {
   return (
@@ -58,6 +59,7 @@ const App = () => {
             element={
               <Layout>
                 <Routes>
+                  <Route path="/contactd" element={<Contactd />} />
                   <Route path="/about" element={<AboutPage />} />
                   <Route
                     path="/viewScholarships"
